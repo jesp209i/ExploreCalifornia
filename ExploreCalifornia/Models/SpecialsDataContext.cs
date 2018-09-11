765856b29de31ef05d182c5f8a768b04efca8d24
jesp209i <jesp209i@edu.eal.dk>
2018-09-12 01:51:37 +0200

    Add project files.

---
+++ ExploreCalifornia/Models/SpecialsDataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.Models
+{
+    public class SpecialsDataContext : DbContext
+    {
+        public DbSet<Special> Specials { get; set; }
+        public SpecialsDataContext(DbContextOptions<SpecialsDataContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public IEnumerable<Special> GetMonthlySpecials() {
+            return Specials.ToArray();
+        }
+    }
+}
---
+++ ExploreCalifornia/api/CommentsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCalifornia.api
+{
+    [Route("api/posts/{postKey}/comments")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly BlogDataContext _db;
+
+        public CommentsController(BlogDataContext db)
+        {
+            _db = db;
+        }
+        // GET: api/Comments
+        [HttpGet]
+        public IQueryable<Comment> Get(string postKey)
+        {
+            return _db.Comments.Where(x=>x.Post.Key == postKey);
+        }
+
+        // GET: api/Comments/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Comments
+        [HttpPost]
+        public Comment Post(string postKey, [FromBody] Comment comment)
+        {
+            var post = _db.Posts.FirstOrDefault(x => x.Key == postKey);
+            if (post == null)
+                return null;
+
+            comment.Post = post;
+            comment.Posted = DateTime.Now;
+            comment.Author = User.Identity.Name;
+
+            _db.Comments.Add(comment);
+            _db.SaveChanges();
+
+            return comment;
+        }
+
+        // PUT: api/Comments/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
